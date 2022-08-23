f484a1680916641a12be4cd1803204fbbe7e3bdd
taiki011609 <s.taiki011609@gmail.com>
2022-08-24 00:09:41 +0900

    add: Atomコンポーネント, Story作成

---
+++ src/components/atoms/Badge/index.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+const BadgeWrapper = styled.div<{ backgroundColor: string }>`
+  border-radius: 20px;
+  height: 20px;
+  min-width: 20px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`
+
+// バッジ内のテキスト
+const BadgeText = styled.p`
+  color: white;
+  font-size: 11px;
+  user-select: none;
+`
+
+/**
+ * バッジのテキスト
+ * バッジの色
+ */
+interface BadgeProps {
+  content: string
+  backgroundColor: string
+}
+
+/**
+ * バッジ
+ */
+const Badge = ({ content, backgroundColor }: BadgeProps) => {
+  return (
+    <BadgeWrapper backgroundColor={backgroundColor}>
+      <BadgeText>{content}</BadgeText>
+    </BadgeWrapper>
+  )
+}
+
+export default Badge
